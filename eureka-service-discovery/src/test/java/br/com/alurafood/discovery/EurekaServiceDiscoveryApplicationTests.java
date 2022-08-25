a6452da5dba4a2ffc700830eb5c98e9061e8740e
Rodrigo Lúcio <luciodigo@gmail.com>
2022-08-24 22:48:37 -0300

    Criado Eureka service discovery

---
+++ eureka-service-discovery/src/test/java/br/com/alurafood/discovery/EurekaServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
