0336c0e96477f1fb9f66321df819af32b859179f
Rodrigo Lúcio <luciodigo@gmail.com>
2022-08-25 18:15:48 -0300

    Update Utils.java

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/shared/util/Utils.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/shared/util/Utils.java
@@ -27,11 +27,10 @@ public class Utils {
 
     private String[] getNullPropertyNames (Object source) {
         final BeanWrapper src = new BeanWrapperImpl(source);
-        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
         Set<String> emptyNames = new HashSet<>();
-        for(java.beans.PropertyDescriptor pd : pds) {
-            Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue == null) emptyNames.add(pd.getName());
+        for(java.beans.PropertyDescriptor descriptor : src.getPropertyDescriptors()) {
+            Object srcValue = src.getPropertyValue(descriptor.getName());
+            if (srcValue == null) emptyNames.add(descriptor.getName());
         }
         String[] result = new String[emptyNames.size()];
         return emptyNames.toArray(result);
