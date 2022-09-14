e2674122bdfc606db48cfdcc3163e389439fb6ac
Rodrigo Lúcio <luciodigo@gmail.com>
2022-09-13 23:18:45 -0300

    Adicionado JsonInclude.Include.NON_NULL na classe Mensagem

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/infra/exception/Mensagem.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/infra/exception/Mensagem.java
@@ -1,10 +1,13 @@
 package br.com.alurafood.pagamentos.infra.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Mensagem {
 
 	private String mensagem;
