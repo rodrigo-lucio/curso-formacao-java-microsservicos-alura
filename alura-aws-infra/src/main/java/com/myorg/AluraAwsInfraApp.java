3af49cfc8d16c2cc17ed71af244c5f4ee7949aa2
Rodrigo Lúcio <luciodigo@gmail.com>
2022-09-26 22:45:19 -0300

    Criado o projeto com CDK

---
+++ alura-aws-infra/src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -0,0 +1,23 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+import java.util.Arrays;
+
+public class AluraAwsInfraApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        AluraVpcStack vpcStack = new AluraVpcStack(app, "vpc");
+
+        AluraClusterStack clusterStack = new AluraClusterStack(app, "cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
+
+        AluraServiceStack serviceStack = new AluraServiceStack(app, "service", clusterStack.getCluster());
+        serviceStack.addDependency(clusterStack);
+        app.synth();
+    }
+}
+
---
+++ alura-aws-infra/src/main/java/com/myorg/AluraServiceStack.java
@@ -0,0 +1,39 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.ICluster;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.constructs.Construct;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+
+public class AluraServiceStack extends Stack {
+    public AluraServiceStack(final Construct scope, final String id, final Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public AluraServiceStack(final Construct scope, final String id, final StackProps props, final Cluster cluster) {
+        super(scope, id, props);
+
+        ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
+                .serviceName("alura-service-ola")
+                .cluster(cluster)           // Required
+                .cpu(512)                   // Default is 256
+                .desiredCount(2)            // Default is 1
+                .listenerPort(8080)
+                .assignPublicIp(Boolean.TRUE)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .image(ContainerImage.fromRegistry("jacquelineoliveira/ola:1.0"))
+                                .containerPort(8080)
+                                .containerName("app_ola")
+                                .build())
+                .memoryLimitMiB(1024)       // Default is 512
+                .publicLoadBalancer(true)   // Default is true
+                .build();
+    }
+}
