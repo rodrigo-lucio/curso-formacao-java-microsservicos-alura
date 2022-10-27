a8751280d669eb1b2047cd0459418faf15c6a186
Rodrigo Lúcio <luciodigo@gmail.com>
2022-10-26 23:27:26 -0300

    Ajustado exception para testar dead letter

--- avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
+++ avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
@@ -1,5 +1,6 @@
 package br.com.alurafood.avaliacao.avaliacao.amqp;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,12 @@ import br.com.alurafood.avaliacao.avaliacao.dto.PagamentoDTO;
 @Component
 public class AvaliacaoListener {
 
-    @RabbitListener(queues = PagamentoEventsConstants.QUEUE_PAGAMENTO_EVENTS)
+    @RabbitListener(queues = PagamentoEventsConstants.QUEUE_PAGAMENTO_EVENTS) // concurrency = "100-100" = lê de 100 em 100
     public void recebeMensagem(@Payload PagamentoDTO pagamento) {
+        System.out.println("lendo pagamento events");
+        if(!StringUtils.isNumeric(pagamento.getNumero())) {
+            throw new RuntimeException("Número do cartão inválido");
+        }
         System.out.println(PagamentoEventsConstants.QUEUE_PAGAMENTO_EVENTS + pagamento.toString());
     }
 }
