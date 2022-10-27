7f07755621bb42dfba92bf45cfe017729aca442b
Rodrigo Lúcio <luciodigo@gmail.com>
2022-10-26 23:29:04 -0300

    Configurado dead letter exchange e dead letter queue para a fila de avaliações

--- avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoAMQPConfiguration.java
+++ avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoAMQPConfiguration.java
@@ -6,9 +6,12 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -30,12 +33,13 @@ public class AvaliacaoAMQPConfiguration {
         return  rabbitTemplate;
     }
 
-    @Bean
-    public Queue filaDetalhesAvaliacao() {
-        return QueueBuilder
-                .durable(PagamentoEventsConstants.QUEUE_PAGAMENTO_EVENTS)
-                .build();
-    }
+//    @Bean
+//    public RabbitListenerContainerFactory<SimpleMessageListenerContainer> prefetchTenRabbitListenerContainerFactory(ConnectionFactory rabbitConnectionFactory) {
+//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+//        factory.setConnectionFactory(rabbitConnectionFactory);
+//        factory.setPrefetchCount(5);
+//        return factory;
+//    }
 
     @Bean
     public FanoutExchange fanoutExchange() {
@@ -45,10 +49,39 @@ public class AvaliacaoAMQPConfiguration {
     }
 
     @Bean
-    public Binding bindPagamentoPedido(FanoutExchange fanoutExchange) {
+    public Queue queuePagamentoEvents() {
+        return QueueBuilder
+                .durable(PagamentoEventsConstants.QUEUE_PAGAMENTO_EVENTS)
+                .deadLetterExchange(PagamentoEventsConstants.EXCHANGE_PAGAMENTO_EVENTS_DEAD_LETTER) // Direciona automaticamente as mensagem para essa deadletter em caso de exception
+                .build();
+    }
+
+    @Bean
+    public FanoutExchange deadLetterExchange() {
+        return ExchangeBuilder
+                .fanoutExchange(PagamentoEventsConstants.EXCHANGE_PAGAMENTO_EVENTS_DEAD_LETTER)
+                .build();
+    }
+
+    @Bean
+    public Queue queuePagamentoEventsDeadLetter() {
+        return QueueBuilder
+                .durable(PagamentoEventsConstants.QUEUE_PAGAMENTO_EVENTS_DEAD_LETTER)
+                .build();
+    }
+
+    @Bean
+    public Binding bindPagamentoEvents() {
         return BindingBuilder
-                .bind(filaDetalhesAvaliacao())
+                .bind(queuePagamentoEvents())
                 .to(fanoutExchange());
+    }
+
+    @Bean
+    public Binding bindPagamentoEventsDeadLetter() {
+        return BindingBuilder
+                .bind(queuePagamentoEventsDeadLetter())
+                .to(deadLetterExchange());
     }
 
     @Bean
--- avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/PagamentoEventsConstants.java
+++ avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/PagamentoEventsConstants.java
@@ -6,6 +6,8 @@ import lombok.experimental.UtilityClass;
 public class PagamentoEventsConstants {
 
     public static final String EXCHANGE_PAGAMENTO_EVENTS = "pagamento.events";
+    public static final String EXCHANGE_PAGAMENTO_EVENTS_DEAD_LETTER = "pagamento.events-dead-letter";
     public static final String QUEUE_PAGAMENTO_EVENTS = "pagamento.events.pagamento-criado.avaliacao";
+    public static final String QUEUE_PAGAMENTO_EVENTS_DEAD_LETTER = "pagamento.events.pagamento-criado.avaliacao-dead-letter";
 
 }
